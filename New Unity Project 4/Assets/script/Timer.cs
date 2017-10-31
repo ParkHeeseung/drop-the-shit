329fc8ac8dc926089703fc68df21f42d6dbeb3bf
DESKTOP-IHINU5M\kch <rlacjfgus2@gmail.com>
2017-10-31 16:40:45 +0900

    오늘도 푸쉬합니당~

---
+++ New Unity Project 4/Assets/script/Timer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+
+    public float TimeCost;
+
+    void Start()
+    {
+        TimeCost = 30f;
+    }
+
+    void Update()
+    {
+        Debug.Log("error!!!!");
+        if (TimeCost != 0)
+        {
+            TimeCost -= Time.deltaTime;
+            if (TimeCost <= 0)
+                TimeCost = 0;
+
+
+        }
+        int t = Mathf.FloorToInt(TimeCost);
+        Text TimeCount = GetComponent<Text>();
+        TimeCount.text = "남은 시간 : " + t.ToString();
+    }
+
+
+}
