2d91ed8fb4f4174ae1b33743a021b75b40ffb04e
yong4980 <31884741+yong4980@users.noreply.github.com>
2017-11-06 23:02:28 +0900

    포탈, 스테이지 넘기기 구현

---
+++ Assets/script/PotalIn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotalIn : MonoBehaviour {
+
+    public Transform TranslatePosition;
+    
+	void OnTriggerEnter(Collider _col)
+    {
+        if(_col.gameObject.tag == "Player")
+        {
+            Transform ParentTransform = _col.transform;
+            while(true)
+            {
+                if (ParentTransform.parent == null)
+                    break;
+                else
+                    ParentTransform = ParentTransform.parent;
+       
+            }
+            ParentTransform.position = TranslatePosition.position;
+            ParentTransform.rotation = TranslatePosition.rotation;
+        }
+    }
+}
