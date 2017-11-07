3680d0a4f152fb4ec0013385dff0edc8ea3eaf93
yong4980 <31884741+yong4980@users.noreply.github.com>
2017-11-07 15:12:23 +0900

    Start버튼 수정

--- Assets/script/StartNextScene.cs
+++ Assets/script/StartNextScene.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class changeScene : MonoBehaviour
+public class StartNextScene : MonoBehaviour
 {
 
     public void SceneChange1()
