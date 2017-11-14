6d54c6ccc3b60e685ecdff0df119ff49a7eea953
yong4980 <31884741+yong4980@users.noreply.github.com>
2017-11-14 15:41:15 +0900

    GameOver 구현

--- Assets/script/Gameover.cs
+++ Assets/script/Gameover.cs
@@ -18,6 +18,6 @@ public class Gameover : MonoBehaviour {
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Respawn")
-            SceneManager.LoadScene(0,LoadSceneMode.Single);
+            SceneManager.LoadScene(10,LoadSceneMode.Single);
     }
 }
