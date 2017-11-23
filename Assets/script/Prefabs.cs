17e5a1a764ac04755e6a1433fc08bfdd030fa874
yong4980 <31884741+yong4980@users.noreply.github.com>
2017-11-23 18:30:03 +0900

    11.23 수정 끝

--- Assets/script/Prefabs.cs
+++ Assets/script/Prefabs.cs
@@ -12,9 +12,10 @@ public class Prefabs : MonoBehaviour
 
     public void newObject()
     {
-        GameObject createdObject = Instantiate(prefab, new Vector3(0, 24, -8), Quaternion.identity);
+        GameObject createdObject = Instantiate(prefab, new Vector3(0, 25, 0), Quaternion.identity);
         createdObject.transform.position = new Vector3(Random.Range(-80, 120), Random.Range(25, 50), Random.Range(-30, 30));
         createdObject.transform.rotation = new Quaternion(10, 20, 0, 0);
+        
     }
 
     void Start()
