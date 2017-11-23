8145db56cafb01c73a89660d2ca68d7a886a28b8
yong4980 <31884741+yong4980@users.noreply.github.com>
2017-11-23 17:48:14 +0900

    펭귄이 똥을 싼다~

--- Assets/script/BirdPrefab.cs
+++ Assets/script/BirdPrefab.cs
@@ -2,45 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Prefabs : MonoBehaviour
-{
+public class BirdPrefab : MonoBehaviour {
+
     public GameObject prefab;
     public float gridX = 5f;
     public float gridY = 5f;
     public float spacing = 2f;
-  
 
     public void newObject()
     {
         GameObject createdObject = Instantiate(prefab, new Vector3(0, 24, -8), Quaternion.identity);
-        createdObject.transform.position = new Vector3(Random.Range(-80, 120), Random.Range(25, 50), Random.Range(-30, 30));
-        createdObject.transform.rotation = new Quaternion(10, 20, 0, 0);
+        createdObject.transform.position = new Vector3(Random.Range(-40, 40), Random.Range(25, 50), Random.Range(-10, 10));
+       // createdObject.transform.rotation = new Quaternion(10, 20, 0, 0);
     }
 
     void Start()
     {
-        prefab = Resources.Load("shit") as GameObject;
-
+        prefab = Resources.Load("Penguin") as GameObject;
         //Prefabs obj = Instantiate(shit);
 
 
         StartCoroutine(newobject());
     }
 
-        IEnumerator newobject(){
-        while (true)
+    IEnumerator newobject(){
+        for (int i = 0; i < 200; i++)
         {
-            for (int i=0; i<1; i++)
-                newObject();
-      
-            yield return new WaitForSeconds(0.7f);
+            newObject();
         }
-        }
-    
+        yield return new WaitForSeconds(0.7f);
+    }
 
     void Update()
     {
-        
+
     }
- 
+
 }
--- Assets/script/MoveRandom.cs
+++ Assets/script/MoveRandom.cs
@@ -16,20 +16,34 @@ public class MoveRandom : MonoBehaviour {
 
         while (true)
         {
-            float dir1 = Random.Range(-15f, 15f);
-            float dir2 = Random.Range(-6f, 6f);
+        
+            if (obj.position.x > 50)
+            {
+                obj.transform.Rotate(0, 180, 0);
+                obj.velocity = -obj.transform.right * 15;
+            }
+            else if (obj.position.x < -70)
+            {
+                obj.transform.Rotate(0, 180, 0);
+                obj.velocity = -obj.transform.right * 15;
+            }
+            else if (obj.position.z > 30)
+            {
+                obj.transform.Rotate(0, 180, 0);
+                obj.velocity = -obj.transform.right * 15;
+            }
+            else if (obj.position.z < -30)
+            {
+                obj.transform.Rotate(0, 180, 0);
+                obj.velocity = -obj.transform.right * 15;
+            }
+            else
+            {
+                obj.transform.Rotate(0, Random.Range(0, 360), 0);
+                obj.velocity = -obj.transform.right * Random.Range(5f, 10f);
+            }
 
-            obj.velocity = new Vector3(dir1, 0, dir2);
-
-            obj.transform.rotation = new Quaternion(0, Random.Range(0, 90), 0, 0);
-
-            //if(obj.position.x >20)
-
-            
-            //if (obj.position.x > 20 || obj.position.z > 20 || obj.position.x < -20 || obj.position.z < -20)
-              //  obj.transform.position= new Vector3(0, 7, 0);
-
-            yield return new WaitForSeconds(1);
+            yield return new WaitForSeconds(2);
         }
     }
 }
