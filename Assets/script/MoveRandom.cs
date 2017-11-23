a1f17b6eaf1a4a2bd03bae4016c20e6bb8bada1f
yong4980 <31884741+yong4980@users.noreply.github.com>
2017-11-23 15:44:17 +0900

    펭귄

--- Assets/script/MoveRandom.cs
+++ Assets/script/MoveRandom.cs
@@ -16,11 +16,18 @@ public class MoveRandom : MonoBehaviour {
 
         while (true)
         {
-            float dir1 = Random.Range(-10f, 0);
-            float dir2 = Random.Range(-3f, 3f);
+            float dir1 = Random.Range(-15f, 15f);
+            float dir2 = Random.Range(-6f, 6f);
 
             obj.velocity = new Vector3(dir1, 0, dir2);
+
+            obj.transform.rotation = new Quaternion(0, Random.Range(0, 90), 0, 0);
+
+            //if(obj.position.x >20)
+
             
+            //if (obj.position.x > 20 || obj.position.z > 20 || obj.position.x < -20 || obj.position.z < -20)
+              //  obj.transform.position= new Vector3(0, 7, 0);
 
             yield return new WaitForSeconds(1);
         }
