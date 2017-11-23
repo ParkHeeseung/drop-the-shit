66b163b3cd43fcfaa6b739ffb07b1297be697935
yong4980 <31884741+yong4980@users.noreply.github.com>
2017-11-23 13:32:23 +0900

    펭귄 하늘 걸어다녀

---
+++ Assets/script/MoveRandom.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRandom : MonoBehaviour {
+
+    public Rigidbody obj;
+
+	void Start () {
+        StartCoroutine(MoveObject()); 
+	}
+	
+	IEnumerator MoveObject()
+    {
+        obj = GetComponent<Rigidbody>();
+
+        while (true)
+        {
+            float dir1 = Random.Range(-10f, 0);
+            float dir2 = Random.Range(-3f, 3f);
+
+            obj.velocity = new Vector3(dir1, 0, dir2);
+            
+
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
