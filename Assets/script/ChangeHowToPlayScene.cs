56f3b6614650483e780a48149712d31896833fd2
jk-PC\jk <jinkyo1117@naver.com>
2017-11-29 09:33:54 +0900

    게임방법, 게임오버, 게임시작화면 넘어가기

---
+++ Assets/script/ChangeHowToPlayScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeHowToPlayScene : MonoBehaviour
+{
+
+    public void SceneChange3()
+    {
+        SceneManager.LoadScene("start");
+    }
+
+    public void SceneChage4()
+    {
+        SceneManager.LoadScene("HowToPlay");
+    }
+}
+
---
+++ Assets/script/ChangeStartScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeStartScene : MonoBehaviour
+{
+
+    public void SceneChange5()
+    {
+        SceneManager.LoadScene("HowToPlay");
+    }
+
+    public void SceneChage6()
+    {
+        SceneManager.LoadScene("Start");
+    }
+}
+
